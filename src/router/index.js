ee02408db1baa969d8fc8da84e1a2a80d581ac45
xiakk <1712575751@qq.com>
2019-08-25 11:40:09 +0800

    首页

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
-
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import Cuowu from '@/views/404'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/',
+      component: Home,
+      children: [
+
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    },
+    { path: '*', component: Cuowu }
   ]
 })
 
---
+++ src/views/404/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="tu"></div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+    .tu{
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background: url(../../assets/images/404.png) no-repeat center / cover;
+    }
+</style>
